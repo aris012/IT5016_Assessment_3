cf35fbd8f3a26644c12465feab354fe17054af45
Aris <165860478+aris012@users.noreply.github.com>
2024-04-16 14:10:49 +1200

    Add files via upload

---
+++ Project Codes/List.py
@@ -0,0 +1,21 @@
+def show_hello(param):
+    print("Hello there, the number of times this"
+          "function is called is {0}!!!\n\n".format(param))
+    
+counter=0
+print("Testing my second user defined function...\n\n")
+
+counter+=1
+show_hello(counter)
+
+counter+=1
+show_hello(counter)
+
+counter+=1
+show_hello(counter)
+
+counter+=1
+show_hello(counter)
+
+counter+=1
+show_hello(counter)
